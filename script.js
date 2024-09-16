05000b16c4da530f5a209919e3052ee06e93295e
nikhil8851 <74211221+nikhil8851@users.noreply.github.com>
2024-09-17 01:01:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,32 +3,20 @@ const container = document.querySelector('.container');
 for (let i = 0; i < 800; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
-    const hoverColor = getRandomColor(); // Generate random hover color
     square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = hoverColor;
+        square.style.backgroundColor = getRandomColor();
         setTimeout(() => {
-            square.style.backgroundColor = ''; // Reset to original color
-        }, 1000); // Remove hovered color after 1 second
+            square.style.backgroundColor = '#000';
+        }, 1000);
     });
     container.appendChild(square);
 }
 
-
-
-
-
-squares.forEach((square) => {
-  square.addEventListener('mouseover', () => {
-    square.style.backgroundColor = getRandomColor(); // Change background color on hover
-  });
-
-  square.addEventListener('mouseout', () => {
-    square.style.backgroundColor = 'rgb(240, 240, 240)'; // Reset background color on mouseout
-  });
-});
-
-// Function to generate random color
 function getRandomColor() {
-  const colors = ['#FF69B4', '#33CC33', '#6666FF', '#CC33CC', '#0099CC']; // Add more colors as needed
-  return colors[Math.floor(Math.random() * colors.length)];
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
